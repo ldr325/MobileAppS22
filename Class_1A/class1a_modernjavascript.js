3f0eaef82f95496f49822e43e65cde9d428f29f4
LeoR <ldr325@nyu.edu>
2022-02-02 16:20:03 -0500

    added folders

---
+++ Class_1A/class1a_modernjavascript.js
@@ -0,0 +1,38 @@
+/*Assignment: Setting up your dev environment
+
+For next class, please install all the prerequisite tools and software to be ready to begin mobile application development on your computer and phone.
+
+These tools will facilitate modern, iterative application development on both your computer and your phones and allow you to debug, test and publish you applications easily.
+
+1. Signup for GitHub
+
+Sign up for a GitHub account here.
+
+2. Install Git on development computer
+
+Follow the installation process for your OS here.
+
+3. Install Visual Studio Code
+
+Download Visual Studio Code here and follow the installation. Install the VS Code React Native Extensions from here.
+
+4. Install Node.js and the Node Package Manager (npm)
+
+Download and install Node.js from here, the installation includes npm.
+
+5. Install yarn as Package Manager by typing into command line
+
+npm install --global yarn
+
+6. Signup for Expo, install Expo and the React-Native Command Line interface
+
+Signup for an Expo account here.
+
+Install expo-cli.
+
+npm install --global expo-cli
+
+7. If you are on Mac, download Xcode
+
+Install Xcode commandline tools
+xcode-select –install*/
